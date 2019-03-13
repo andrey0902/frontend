c2de34ccd9dd0bb1d2a7c8d9636fe753cc990994
Gleb Grudiev <grudiev.g@gmail.com>
2019-03-13 12:52:20 +0200

    Add guard for create-iteration page / add base url to environments file

--- src/app/profile/profile-routing.module.ts
+++ src/app/profile/profile-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ProfileComponent} from './profile.component';
 import {CreateIterationComponent} from './create-iteration/create-iteration.component';
+import {CreateIterationGuard} from '../core/services/create-iteration.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: ':id', pathMatch: 'full' },
   { path: ':id', component: ProfileComponent },
-  { path: ':id/create-iteration', component: CreateIterationComponent },
+  { path: ':id/create-iteration', component: CreateIterationComponent, canActivate: [CreateIterationGuard] },
 ];
 
 @NgModule({
