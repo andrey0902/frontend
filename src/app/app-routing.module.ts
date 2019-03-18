dd1ecbd42835f39d8451ee3a643052819c9caedf
andrey0902 <andreypavlov1706@gmail.com>
2019-03-18 17:37:25 +0200

    add guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MentorsGuard } from './mentorship/shared/services/mentors-guard.service';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'mentorship' },
-  { path: 'mentorship',  loadChildren: './mentorship/mentorship.module#MentorshipModule' },
+  { path: 'mentorship',  loadChildren: './mentorship/mentorship.module#MentorshipModule', canActivate: [MentorsGuard] },
   { path: 'profile',  loadChildren: './profile/profile.module#ProfileModule' },
   { path: 'plan', loadChildren: './personal-plan/personal-plan.module#PersonalPlanModule'},
   { path: '**', redirectTo: 'mentorship' },
