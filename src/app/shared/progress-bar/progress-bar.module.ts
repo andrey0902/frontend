41ddf9a74f16af1124bd3c083b5aac0e0f58efa7
andrey0902 <andreypavlov1706@gmail.com>
2019-03-19 16:12:07 +0200

    change progress ipr

--- src/app/shared/progress-bar/progress-bar.module.ts
+++ src/app/shared/progress-bar/progress-bar.module.ts
@@ -1,12 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ProgressBarComponent} from './progress-bar/progress-bar.component';
+import { MatProgressBarModule } from '@angular/material';
 
 @NgModule({
   declarations: [ProgressBarComponent],
   exports: [ProgressBarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatProgressBarModule,
   ]
 })
 export class ProgressBarModule {
