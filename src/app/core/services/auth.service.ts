a6d66b4544d129800998ad49f959468c91bae5ec
ulyanki <ulyanastryuk@gmail.com>
2019-03-21 09:58:13 +0200

    refactoring

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -10,7 +10,7 @@ export class AuthService {
   constructor(private cookieService: CookieService) {
 
     // TODO: Remove this in the future. This is a temporary solution
-    // this.cookieService.set( 'ltp.token', '482d2a74d005cc435f6f1d535d1a6ffa095a7d19' );
+     this.cookieService.set( 'ltp.token', '482d2a74d005cc435f6f1d535d1a6ffa095a7d19' );
   }
 
   getAuthToken() {
