9d5b4f6eed70f96d4c6787c9c8df77b1abc313d0
ulyanki <ulyanastryuk@gmail.com>
2019-04-17 15:50:58 +0300

    token comment

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -10,7 +10,7 @@ export class AuthService {
   constructor(private cookieService: CookieStorageService) {
 
     // TODO: Remove this in the future. This is a temporary solution
-    // this.cookieService.token = '67306d5ffe6ed329783c1fdcb131db4d8872c4b5';
+    //  this.cookieService.token = 'f2e3698d0c0c2a9a01ca3e99f6effd459da693f4';
   }
 
   getAuthToken() {
