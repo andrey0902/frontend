9c06fc64abc631ae71874a221da03ba0d8f06a56
ulyanki <ulyanastryuk@gmail.com>
2019-04-18 17:19:27 +0300

    change color and add validation for dialog close

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -10,7 +10,7 @@ export class AuthService {
   constructor(private cookieService: CookieStorageService) {
 
     // TODO: Remove this in the future. This is a temporary solution
-   //  this.cookieService.token = '66b8f4dfadebf7ffc0b8b29bdde4c04040b5e834';
+    // this.cookieService.token = '2a72a610b6ff740f7f1560701d8bd45659295c9c';
   }
 
   getAuthToken() {
--- src/app/shared/tree/tree.component.ts
+++ src/app/shared/tree/tree.component.ts
@@ -242,7 +242,8 @@ export class TreeComponent implements OnChanges, AfterViewInit {
 
   addComment(node: ItemFlatNode) {
     this.dialogService.openCommentDialog(node.comment, this.editLevel !== 2, (request => {
-      if (request !== node.comment && this.editLevel === 2) {
+      console.log(request);
+      if (request !== undefined && request !== node.comment && this.editLevel === 2) {
         const data = this.flatNodeMap.get(node);
         data.comment = (request || '').trim().length !== 0 ? request : undefined;
         this.editItem.emit(data);
