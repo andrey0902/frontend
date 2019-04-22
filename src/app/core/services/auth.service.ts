59f64bfb0d2703c497b1950b4114875a1dac6b0e
ulyanki <ulyanastryuk@gmail.com>
2019-04-22 12:09:43 +0300

    add pipe and styles for test project link

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -10,7 +10,7 @@ export class AuthService {
   constructor(private cookieService: CookieStorageService) {
 
     // TODO: Remove this in the future. This is a temporary solution
-    // this.cookieService.token = '294ed3b2d75ffb539f733c0271ce29c56056ffd0';
+    // this.cookieService.token = 'd31cfad5c8317351a554ea5c57a2cbdbf20df250';
   }
 
   getAuthToken() {
